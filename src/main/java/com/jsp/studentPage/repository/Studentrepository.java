85d26b04cb4e0a8a570f32ec0dc05321a3bb902c
takkanrohith <145127929+takkanrohith69@users.noreply.github.com>
2023-10-15 19:16:12 +0530

    Delete src directory

--- src/main/java/com/jsp/studentPage/repository/Studentrepository.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.studentPage.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jsp.studentPage.dto.Student;
-
-public interface Studentrepository extends JpaRepository<Student, Integer> {
-
-}
